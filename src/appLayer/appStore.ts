d3c58e93ed60b4ffe94d0906688113138d37b71c
Saveliy <sava2698@gmail.com>
2023-07-05 17:04:00 +0600

    White list added to persistConfig

--- src/appLayer/appStore.ts
+++ src/appLayer/appStore.ts
@@ -3,13 +3,23 @@ import singUpReducer from '@/features/registration/model/signUp-Slice';
 import { TypedUseSelectorHook, useSelector, useDispatch } from 'react-redux';
 
 //REDUX PERSIST
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 //REDUX PERSIST
 const persistConfig = {
   key: 'root',
   storage,
+  whitelist: ['signUp'],
 };
 
 const rootReducer = combineReducers({
@@ -20,6 +30,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
