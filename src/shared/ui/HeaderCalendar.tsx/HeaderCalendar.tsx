683dc02b433009bd29476f68f7ccbaea47e12d13
Saveliy <sava2698@gmail.com>
2023-07-07 13:11:17 +0600

    Header calendar created

---
+++ src/shared/ui/HeaderCalendar.tsx/HeaderCalendar.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import styles from './HeaderCalendar.module.scss';
+
+interface HeaderCalendarProps {}
+
+const HeaderCalendar: FC<HeaderCalendarProps> = ({}) => {
+  return <div>HeaderCalendar</div>;
+};
+
+export default HeaderCalendar;
