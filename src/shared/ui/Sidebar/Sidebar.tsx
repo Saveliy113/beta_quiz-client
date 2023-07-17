d4352dde9e73c95ba7735c4c00cdb91197377d1c
Saveliy <sava2698@gmail.com>
2023-07-17 15:18:23 +0600

    Edited sidebar

--- src/shared/ui/Sidebar/Sidebar.tsx
+++ src/shared/ui/Sidebar/Sidebar.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { Dispatch, FC, SetStateAction, useState } from 'react';
+import { Dispatch, FC, SetStateAction } from 'react';
 import styles from './Sidebar.module.scss';
 import SidebarLink from '../SidebarLink/SidebarLink';
 import { AlignJustify } from 'lucide-react';
 import { BookOpen, Users, ClipboardList, HelpCircle } from 'lucide-react';
-import clsx from 'clsx';
 import { AnimatePresence, motion } from 'framer-motion';
 
 interface SidebarProps {
@@ -23,16 +22,34 @@ const Sidebar: FC<SidebarProps> = ({ isOpened, setIsOpened }) => {
         <AlignJustify />
       </button>
 
-      <div className={clsx(styles.sidebar, !isOpened && styles.closed)}>
-        <SidebarLink href="/lessons" text="Уроки" icon={<BookOpen />} />
-        <SidebarLink href="/mygroups" text="Мои группы" icon={<Users />} />
-        <SidebarLink
-          href="/results"
-          text="Результаты"
-          icon={<ClipboardList />}
-        />
-        <SidebarLink href="/quizes" text="Квизы" icon={<HelpCircle />} />
-      </div>
+      <AnimatePresence>
+        {isOpened && (
+          <motion.div
+            className={styles.sidebar}
+            initial="initialState"
+            animate="animateState"
+            exit="exitState"
+            transition={{ duration: 0.2 }}
+            variants={{
+              initialState: {
+                left: '-10%',
+                width: 0,
+              },
+              animateState: { left: 0, width: '18%' },
+              exitState: { left: '-10%', width: 0 },
+            }}
+          >
+            <SidebarLink href="/lessons" text="Уроки" icon={<BookOpen />} />
+            <SidebarLink href="/mygroups" text="Мои группы" icon={<Users />} />
+            <SidebarLink
+              href="/results"
+              text="Результаты"
+              icon={<ClipboardList />}
+            />
+            <SidebarLink href="/quizes" text="Квизы" icon={<HelpCircle />} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
