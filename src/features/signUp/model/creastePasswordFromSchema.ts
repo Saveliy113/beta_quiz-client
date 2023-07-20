478d01fb0f459bf8668435985233a2d4d98d2ce8
Saveliy <sava2698@gmail.com>
2023-07-20 10:22:16 +0600

    Changed password length on sign up

--- src/features/signUp/model/creastePasswordFromSchema.ts
+++ src/features/signUp/model/creastePasswordFromSchema.ts
@@ -2,8 +2,14 @@ import { z } from 'zod';
 
 export const createPasswordFormSchema = z
   .object({
-    password: z.string().nonempty('Обязательное поле'),
-    confirmPassword: z.string().nonempty('Обязательное поле'),
+    password: z
+      .string()
+      .min(6, 'Пароль должен содержать не менее 6 символов')
+      .nonempty('Обязательное поле'),
+    confirmPassword: z
+      .string()
+      .min(6, 'Пароль должен содержать не менее 6 символов')
+      .nonempty('Обязательное поле'),
   })
   .superRefine(({ password, confirmPassword }, ctx) => {
     if (password !== confirmPassword) {
