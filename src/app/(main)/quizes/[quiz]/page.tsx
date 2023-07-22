b0d9b6f0a431e97c1aef5a0aef59afbc94aa964c
Saveliy <sava2698@gmail.com>
2023-07-22 10:08:17 +0600

    Quiz details page html css done

--- src/app/(main)/quizes/[quiz]/page.tsx
+++ src/app/(main)/quizes/[quiz]/page.tsx
@@ -5,6 +5,7 @@ import DetailsPaper from '@/shared/ui/DetailsPaper/DetailsPaper';
 import ContentPaper from '@/shared/ui/ContentPaper/ContentPaper';
 import CardInfoHeader from '@/shared/ui/CardInfoHeader/CardInfoHeader';
 import { ListTodo } from 'lucide-react';
+import CardRow from '@/shared/ui/CardRow/CardRow';
 
 interface pageProps {}
 
@@ -17,9 +18,20 @@ const page: FC<pageProps> = ({}) => {
         <DetailsPaper title="Тема" text="Проценты" color="violet" />
         <DetailsPaper title="Студентов" text="17 / 20" color="yellow" />
       </div>
-      <ContentPaper>
-        <CardInfoHeader icon={<ListTodo />} text="Результаты учеников" />
-      </ContentPaper>
+
+      <div className={styles.content__wrapper}>
+        <ContentPaper>
+          <CardInfoHeader
+            icon={<ListTodo />}
+            text="Результаты учеников"
+            className={styles.quiz__header}
+          />
+          <CardRow href="#" text="1. Дамир Қайратұлы, 20/20" />
+          <CardRow href="#" text="2. Абдулдаев Мағжан, 18/20" />
+          <CardRow href="#" text="3. Дамир Қайратұлы, 18/20" />
+          <CardRow href="#" text="4. Дуйсен Сара,   17/20" />
+        </ContentPaper>
+      </div>
     </>
   );
 };
